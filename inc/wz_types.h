14f53498dd91360f78e5ef74ad5a55805a666af6
aaa <aaa@ddd.com>
2015-08-23 06:37:01 -0400

    initial version

---
+++ inc/wz_types.h
@@ -0,0 +1,15 @@
+#ifndef __WZ_TYPES_H__
+#define __WZ_TYPES_H__
+
+typedef signed char s8;
+typedef unsigned char u8;
+
+typedef signed short s16;
+typedef unsigned short u16;
+
+typedef signed int s32;
+typedef unsigned int u32;
+
+#endif
+
+
